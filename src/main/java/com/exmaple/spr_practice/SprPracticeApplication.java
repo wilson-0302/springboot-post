2c5cf1076f2c3eeae6a4d4e17a18fef1f0f5a719
wilson-0302 <gksmf030302@handong.ac.kr>
2025-07-25 18:53:53 +0900

    spring boot practice base

---
+++ src/main/java/com/exmaple/spr_practice/SprPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.exmaple.spr_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprPracticeApplication.class, args);
+    }
+
+}
