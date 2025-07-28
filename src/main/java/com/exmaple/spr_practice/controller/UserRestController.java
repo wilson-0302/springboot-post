8405a1ce544f3a35b5320c9feb77c63a58571944
wilson-0302 <gksmf030302@handong.ac.kr>
2025-07-28 14:08:25 +0900

    making user

---
+++ src/main/java/com/exmaple/spr_practice/controller/UserRestController.java
@@ -0,0 +1,41 @@
+package com.exmaple.spr_practice.controller;
+
+import com.exmaple.spr_practice.dto.PostDto;
+import com.exmaple.spr_practice.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/post")
+@RestController
+public class UserRestController {
+
+    final PostService postService;
+
+    @PostMapping("")
+    public ResponseEntity<PostDto.CreateResDto> create(@RequestBody PostDto.CreateReqDto param){
+        return ResponseEntity.ok(postService.create(param));
+    }
+    @PutMapping("")
+    public ResponseEntity<Void> update(@RequestBody PostDto.UpdateReqDto param){
+        postService.update(param);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+    @DeleteMapping("")
+    public ResponseEntity<Void> delete(@RequestBody PostDto.UpdateReqDto param){
+        postService.delete(param);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+    @GetMapping("")
+    public ResponseEntity<PostDto.DetailResDto> detail(PostDto.DetailReqDto param){
+        return ResponseEntity.ok(postService.detail(param));
+    }
+    @GetMapping("/list")
+    public ResponseEntity<List<PostDto.DetailResDto>> list(PostDto.ListReqDto param){
+        return ResponseEntity.ok(postService.list(param));
+    }
+}
